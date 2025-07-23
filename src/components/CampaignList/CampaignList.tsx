8be348e535cbcd6fb1858a4d8fbb381f6de45ecc
yongliang <140040477+sirius-04@users.noreply.github.com>
2025-07-23 13:50:08 +0800

    Update CampaignList.tsx

--- src/components/CampaignList/CampaignList.tsx
+++ src/components/CampaignList/CampaignList.tsx
@@ -1,35 +1,27 @@
-import { useReadContract } from "wagmi";
-import { campaignFactoryConfig } from "@/lib/contracts";
+'use client';
+
+import { useReadContract } from 'wagmi';
+import { campaignFactoryConfig } from '@/lib/contracts';
 
 export default function CampaignList() {
-  const { data, isLoading } = useReadContract({
+  const { data, isPending, error } = useReadContract({
     abi: campaignFactoryConfig.abi,
     address: campaignFactoryConfig.address,
     functionName: 'getDeployedCampaigns',
   });
-  const campaigns = data as `0x${string}`[];
 
-  let content;
+  const campaigns = data as `0x${string}`[] | undefined;
 
-  if (isLoading) {
-    content = (
-      <p>Loading...</p>
-    );
-  } else {
-    content = (
-      <ul>
-        {campaigns.map(campaign => (
-          <li>
-            <a href={`/campaigns/${campaign}`}>{campaign}</a>
-          </li>
-        ))}
-      </ul>
-    );
-  }
+  if (isPending) return <p>Loading...</p>;
+  if (error) return <p style={{ color: 'red' }}>Error: {error.message}</p>;
 
   return (
-    <>
-      {content}
-    </>
-  )
+    <ul>
+      {campaigns?.map((campaign) => (
+        <li key={campaign}>
+          <a href={`/campaigns/${campaign}`}>{campaign}</a>
+        </li>
+      ))}
+    </ul>
+  );
 }
